b4ff09b6ddf277cda82c03eb60da0b3d28c30ff7
MrArtemSid <artem.sid.key@gmail.com>
2023-02-22 01:47:41 +0400

    MLiDM: labs: lab3: Rename {X, Y} -> {A, B} and add (A->B, B->A)

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -7,11 +7,11 @@
         <h1>Лабораторная работа №3</h1>
         <h2>Вариант №14: матрица NxN</h2>
         <form action="" method="POST">
-            Введите множество X через пробел<br>
-            <input type="text" name="masX" id="id_masX" value=""><br>
-            Введите множество Y через пробел<br>
-            <input type="text" name="masY" id="id_masY" value=""><br>
-            Введите матрицу смежности<br>(X - строки, Y - столбцы):<br>
+            Введите множество A через пробел<br>
+            <input type="text" name="masA" id="id_masA" value=""><br>
+            Введите множество B через пробел<br>
+            <input type="text" name="masB" id="id_masB" value=""><br>
+            Введите матрицу смежности<br>(A - строки, B - столбцы):<br>
             <textarea type="text" name="masOfMas" id="id_masOfMas" value=""></textarea><br>
             <br>
             <input type="button" onClick="main()" value="Рассчитать значения">
--- labs/lab3/scripts/functions.js
+++ labs/lab3/scripts/functions.js
@@ -1,88 +1,111 @@
 /**
  * Валидация параметров ввода
+ * @param masA
+ * @param masB
+ * @param masOfMas
  */
-function validateElement(masX, masY, masOfMas)
-{
+function validateElement(masA, masB, masOfMas) {
     let is_valid = true;
 
-    if (masX.length != masY.length) {
-        document.getElementById("result").innerHTML += "Количество элементов массива X не соответствует количеству массива Y <br>";
-        is_valid = false;
-    }
-
-    if (masX.length != new Set(masX).size) {
+    if (masA.length != new Set(masA).size) {
         document.getElementById("result").innerHTML += "Массив X содержит повторения <br>";
         is_valid = false;
     }
 
-    if (masY.length != new Set(masY).size) {
+    if (masB.length != new Set(masB).size) {
         document.getElementById("result").innerHTML += "Массив Y содержит повторения <br>";
         is_valid = false;
     }
 
-    if (masOfMas.length != masY.length || masOfMas.length != masX.length) {
-        document.getElementById("result").innerHTML += "Матрица смежностей не соответсвует массивам <br>";
+    if (masOfMas.length != masB.length) {
+        document.getElementById("result").innerHTML += "Количество строк матрицы смежности не совпадает с массивами <br>";
         is_valid = false;
+    }
+
+    if (masOfMas[0].split(" ").length != masA.length) {
+        document.getElementById("result").innerHTML += "Количество столбцов матрицы смежности не совпадает с массивами <br>";
+        is_valid = false;
+    }
+
+    for (let i = 0; i < masOfMas.length; ++i) {
+        let elem = masOfMas[i].split(" ");
+        if (elem.length != masA.length) {
+            document.getElementById("result").innerHTML += "Неверное количество элементов в " + (i + 1) + " строке <br>";
+            is_valid = false;
+        }
     }
 
     return is_valid;
 }
 
 /**
- * Основная функция
+ * Проверка, являются ли отношения функцией
+ * @param masA
+ * @param masB
+ * @param masOfMas
+ * @param type
  */
-function main() {
-    let masX = document.getElementById("id_masX").value
-    let masY = document.getElementById("id_masY").value
-    let masOfMas = document.getElementById("id_masOfMas").value
-    document.getElementById("result").innerHTML = "Результат: <br>";
-    masX = masX.split(" ");
-    masY = masY.split(" ");
-    masOfMas = masOfMas.split("\n");
-    let isErr = false;
-
-    if (!validateElement(masX, masY, masOfMas)) {
-        return;
-    }
-
+function check(masA, masB, masOfMas, type) {
     let mp = new Map();
-    let is_function = true;
+    let isFunction = true;
+    let x = 0;
+    let y = 0;
     for (let i = 0; i < masOfMas.length; ++i) {
         let elem = masOfMas[i].split(" ");
-        if (elem.length != masX.length) {
-            document.getElementById("result").innerHTML += "Неверное количество элементов в " + i + " строке <br>";
-            isErr = true;
-            continue;
-        }
         for (let j = 0; j < elem.length; ++j) {
+            if (type == "AB")
+                x = j, y = i;
+            else
+                x = i, y = j;
             if (elem[j] == 1) {
-                if (mp.has(masX[i]) == 0)
-                    mp.set(masX[i], new Set());
-                mp.get(masX[i]).add(masY[j]);
-                if (mp.get(masX[i]).size > 1) {
-                    is_function = false;
+                if (mp.has(masA[x]) == 0)
+                    mp.set(masA[x], new Set());
+                mp.get(masA[x]).add(masB[y]);
+                if (mp.get(masA[x]).size > 1) {
+                    isFunction = false;
                 }
             }
         }
     }
 
-    if (isErr) {
+    if (isFunction)
+        for (let elem of mp.keys()) {
+            if (mp.get(elem).size != 1) {
+                isFunction = false
+                break;
+            }
+        }
+    return isFunction;
+}
+
+/**
+ * Основная функция
+ */
+function main() {
+    let masA = document.getElementById("id_masA").value
+    let masB = document.getElementById("id_masB").value
+    let masOfMas = document.getElementById("id_masOfMas").value
+    document.getElementById("result").innerHTML = "Результат: <br>";
+    masA = masA.split(" ");
+    masB = masB.split(" ");
+    masOfMas = masOfMas.split("\n");
+    let IsFunction = true;
+
+    if (!validateElement(masA, masB, masOfMas)) {
         return;
     }
 
-    if (mp.size < masY.length)
-        is_function = false;
-
-    if (is_function)
-        for (let elem of mp.keys()) {
-            if (mp.get(elem).size != 1)
-                is_function = false
-            break;
+    for (let i = 0; i < 2; ++i) {
+        if (i == 1) {
+            IsFunction = check(masA, masB, masOfMas, "BA");
+            document.getElementById("result").innerHTML += "B -> A";
+        } else {
+            IsFunction = check(masB, masA, masOfMas, "AB");
+            document.getElementById("result").innerHTML += "A -> B";
         }
-
-
-    document.getElementById("result").innerHTML = "Данные отношения ";
-    if (!is_function)
-        document.getElementById("result").innerHTML += "не ";
-    document.getElementById("result").innerHTML += "являются функцией";
+        document.getElementById("result").innerHTML += " отношения ";
+        if (!IsFunction)
+            document.getElementById("result").innerHTML += "не ";
+        document.getElementById("result").innerHTML += "являются функцией <br>";
+    }
 }
