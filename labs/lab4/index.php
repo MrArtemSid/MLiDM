38d0beea1671d7b7d34c4e51c2a4c226e2f72bf1
MrArtemSid <artem.sid.key@gmail.com>
2023-02-26 13:34:50 +0400

    MLiDM: labs: lab4: Do not try to output undefined variables
    Fixes:
    PHP Warning:  Undefined array key "mas" in /srv/http/ULSTU/MLiDM/labs/lab4/index.php on line 11
    PHP Stack trace:
    PHP   1. {main}() /srv/http/ULSTU/MLiDM/labs/lab4/index.php:0
    PHP Warning:  Undefined array key "start" in /srv/http/ULSTU/MLiDM/labs/lab4/index.php on line 13
    PHP Stack trace:
    PHP   1. {main}() /srv/http/ULSTU/MLiDM/labs/lab4/index.php:0
    PHP Warning:  Undefined array key "end" in /srv/http/ULSTU/MLiDM/labs/lab4/index.php on line 15
    PHP Stack trace:
    PHP   1. {main}() /srv/http/ULSTU/MLiDM/labs/lab4/index.php:0
    PHP Warning:  Undefined array key "masOfMas" in /srv/http/ULSTU/MLiDM/labs/lab4/index.php on line 18
    PHP Stack trace:
    PHP   1. {main}() /srv/http/ULSTU/MLiDM/labs/lab4/index.php:0

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -8,14 +8,14 @@
         <h2>Вариант №14: неориентированный, матрицей</h2>
         <form action="" method="POST">
             Введите множество вершин через пробел<br>
-            <input type="text" name="mas" value="<?php echo $_POST['mas']; ?>"><br>
+            <input type="text" name="mas" value="<?php if (!empty($_POST["mas"])) echo $_POST['mas']; ?>"><br>
             Введите начальную вершину<br>
-            <input type="text" name="start" value="<?php echo $_POST['start']; ?>"><br>
+            <input type="text" name="start" value="<?php if (!empty($_POST["start"])) echo $_POST['start']; ?>"><br>
             Введите конечную вершину<br>
-            <input type="text" name="end" value="<?php echo $_POST['end']; ?>"><br>
+            <input type="text" name="end" value="<?php if (!empty($_POST["end"])) echo $_POST['end']; ?>"><br>
             Введите весовую матрицу:<br>
             <small>(-1 при отсутствии пути, 0 по главной диагонали)<br></small>
-            <textarea type="text" name="masOfMas" id="id_masOfMas"><?php echo $_POST['masOfMas']; ?></textarea><br>
+            <textarea type="text" name="masOfMas" id="id_masOfMas"><?php if (!empty($_POST["masOfMas"])) echo $_POST['masOfMas']; ?></textarea><br>
             <br>
             <input type="submit" name="button1" class="button" value="Рассчитать значения" />
             <input type="button" name="button2" class="button_active" onclick="location.href=''" value="Очистить"  />
